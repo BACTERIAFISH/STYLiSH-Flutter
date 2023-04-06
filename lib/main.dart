a5815f86ebbab51527c587007977b2ff8bd64b5d
Bacteriafish <you@example.com>
2023-04-06 13:03:44 +0800

    Add product description

--- lib/main.dart
+++ lib/main.dart
@@ -291,6 +291,77 @@ class _DetailPageState extends State<DetailPage> {
                         margin: const EdgeInsets.only(top: 16),
                         child: ProductDetail(),
                       ),
+                      Container(
+                        width: 360,
+                        padding:
+                            const EdgeInsets.only(left: 16, top: 16, right: 16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  margin: const EdgeInsets.only(right: 16),
+                                  child: ShaderMask(
+                                    blendMode: BlendMode.srcIn,
+                                    shaderCallback: (bounds) {
+                                      return const LinearGradient(
+                                        colors: [
+                                          Color(0xFF4B39EF),
+                                          Color(0xFF39D2C0),
+                                        ],
+                                      ).createShader(bounds);
+                                    },
+                                    child: const Text('細部說明'),
+                                  ),
+                                ),
+                                const Expanded(
+                                  child: Divider(
+                                    thickness: 1,
+                                    color: Colors.grey,
+                                  ),
+                                )
+                              ],
+                            ),
+                            const Text(
+                              'O.N.S is all about options, which is why we took our staple polo shirt and upgraded it with slubby linen jersey, making it even lighter for those who prefer their summer style extra-breezy.',
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            Container(
+                              height: 180,
+                              margin: const EdgeInsets.only(top: 16),
+                              child: Image.asset(
+                                'assets/dodoro_bus.png',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Container(
+                              height: 180,
+                              margin: const EdgeInsets.only(top: 16),
+                              child: Image.asset(
+                                'assets/dodoro_bus.png',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Container(
+                              height: 180,
+                              margin: const EdgeInsets.only(top: 16),
+                              child: Image.asset(
+                                'assets/dodoro_bus.png',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Container(
+                              height: 180,
+                              margin: const EdgeInsets.only(top: 16),
+                              child: Image.asset(
+                                'assets/dodoro_bus.png',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
                     ],
                   );
                 } else {
