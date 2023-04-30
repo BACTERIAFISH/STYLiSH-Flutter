cf7084d7358dede6406c966b0c4c8bd2aee08b36
Bacteriafish <you@example.com>
2023-04-30 12:50:45 +0800

    import google_maps_flutter

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,8 @@ import Flutter
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+
+        GMSServices.provideAPIKey("AIzaSyCmVvg9BQ2Brst4zU_VNmPEqcbyEkIFCeA")
         
         let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         
